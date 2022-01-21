c97646fce7335a93a41c45f2689c8d2600c332cb
Nate Mueller <inathan.m@gmail.com>
2022-01-20 21:50:16 -0800

    g

--- contracts/WineBottleV1.sol
+++ contracts/WineBottleV1.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./UriUtils.sol";
 import "./IAddressStorage.sol";
-import "hardhat/console.sol";
 
 interface CellarContract {
     function cellarTime(uint256 _tokenID) external view returns (uint256);
